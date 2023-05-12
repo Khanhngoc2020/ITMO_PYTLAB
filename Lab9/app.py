8d8e7a49da34827256a33cb77d0b5cd03ab1f2cf
Khanhngoc2020 <khanhngocn45@gmail.com>
2023-05-12 18:55:53 +0300

    add Lab9

---
+++ Lab9/app.py
@@ -0,0 +1,57 @@
+
+import flask
+from flask import Flask, request, redirect, url_for
+
+from flask_sqlalchemy import SQLAlchemy
+
+DB_USERNAME = 'postgres'
+DB_PASSWORD = 'khanhngoc2020'
+DB_NETWORK = 'localhost'
+DB_DATABASE = 'postgres'
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_NETWORK}/{DB_DATABASE}'
+db = SQLAlchemy(app)
+
+
+class Portfolio(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    portfolio_name = db.Column(db.String, nullable=False)
+    portfolio_link = db.Column(db.String, nullable=False)
+
+    def __init__(self, portfolio_name, portfolio_link):
+        self.portfolio_name = portfolio_name
+        self.portfolio_link = portfolio_link
+
+
+@app.route('/', methods=['GET'])
+def index():
+    error = request.args.get('error', '')
+    return flask.render_template('index.html', portfolios=Portfolio.query.all(), error=error)
+
+
+@app.route('/portfolio', methods=['POST'])
+def add_new_portfolio():
+    portfolio_name = request.form['portfolio_name']
+    portfolio_link = request.form['portfolio_link']
+
+    if portfolio_name == '' or portfolio_link == '':
+        return redirect(url_for('index', error='Пожалуйста, введите название города и дату посещения'))
+
+    db.session.add(Portfolio(portfolio_name, portfolio_link))
+    db.session.commit()
+
+    return redirect(url_for('index')) 
+
+
+@app.route('/portfolio', methods=['DELETE'])
+def delete_all_comments():
+    Portfolio.query.delete()
+    db.session.commit()
+    return "Success"
+
+
+with app.app_context():
+    db.create_all()
+
+app.run()
